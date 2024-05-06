bd9f122aac1902428c9a389cf6cdcec4645aead9
burhandhar6004 <burhaneducation6004@gmail.com>
2024-05-06 09:06:08 +0530

    clear all option change

--- index.js
+++ index.js
@@ -1,66 +1,60 @@
-
 // select All elements first
 
-const formSelect = document.querySelector("form")
-const ulSelect = document.querySelector("ul")
-const inputSelector = document.querySelector("input")
-const buttonSelect = document.querySelector(".delete")
+const formSelect = document.querySelector("form");
+const ulSelect = document.querySelector("ul");
+const inputSelector = document.querySelector("input");
+const buttonSelect = document.querySelector(".delete");
 // const container = document.querySelector(".container")
 // const ul = document.querySelector("ul")
 
 // create a function for submit form
 
 const savetodo = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
+  // create element
+  let listSelector = document.createElement("li");
+  const buttonSelector = document.createElement("button");
+  buttonSelector.innerHTML = '<i class="fa-solid fa-trash"></i>';
 
-    // create element
-    let listSelector = document.createElement("li")
-    const buttonSelector = document.createElement("button")
-    buttonSelector.innerHTML='<i class="fa-solid fa-trash"></i>'
+  // inser in element
+  listSelector.innerText = inputSelector.value;
+  listSelector.className = "list-group-item ";
 
-    // inser in element
-    listSelector.innerText = inputSelector.value;
-    listSelector.className = "list-group-item ";
+  buttonSelector.className = "btn btn-sm btn-danger lidel float-end";
 
-    buttonSelector.className = "btn btn-sm btn-danger lidel float-end";
+  listSelector.appendChild(buttonSelector);
+  ulSelect.appendChild(listSelector);
 
-    listSelector.appendChild(buttonSelector)
-    ulSelect.appendChild(listSelector)
+  // console.log(listSelector)
+  formSelect.reset();
+};
 
-    console.log(listSelector)
-    formSelect.reset();
+formSelect.addEventListener("submit", savetodo);
 
-}
+const todoDeleted = (e) => {
+  if (e.target.className.includes("lidel"));
+  {
+    let listSelector = e.target.parentElement;
+    ulSelect.removeChild(listSelector);
+    // console.log(e.target.className);
+  }
+};
 
-formSelect.addEventListener("submit", savetodo)
+ulSelect.addEventListener("click", todoDeleted);
 
+const clearAll = (e) => {
+  if (e.target.className.includes("delete"));
+  {
+    let allList = document.querySelectorAll("li");
+    ulSelect.remove();
+  }
+};
 
-const todoDeleted = (e)=>{
-    if(e.target.className.includes("lidel"));{
-        let listSelector = e.target.parentElement;
-        ulSelect.removeChild(listSelector)
-        // console.log(e.target.className);
-    }
-}
+buttonSelect.addEventListener("click", clearAll);
 
-ulSelect.addEventListener("click", todoDeleted)
-
-
-
-// const clearAll = (e) =>{
-//     if(e.target.className.includes("delete"));{
-// let buttonSelect = e.target.nextSibling;
-// ulSelect.removeChild(ul)
-//     }
-// }
-
-// ul.addEventListener("click" , clearAll)
-
-buttonSelect.addEventListener("click" , ()=>{
-  console.log(ul.children)
-  let alllist = document.querySelectorAll("li")
-  ulSelect.remove()
-})
-
-
+// buttonSelect.addEventListener("click" , ()=>{
+//   console.log(ul.children)
+//   let alllist = document.querySelectorAll("li")
+//   ulSelect.remove()
+// })
